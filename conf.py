7cd6a3972139eb90cafe8efb9d0dee1aea09c065
Santos Gallegos <santos_g@outlook.com>
2018-05-30 02:33:47 -0500

    Add ci

--- conf.py
+++ conf.py
@@ -1159,7 +1159,7 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 # SHOW_SOURCELINK = True
 # Copy the source files for your pages?
 # Setting it to False implies SHOW_SOURCELINK = False
-# COPY_SOURCES = True
+COPY_SOURCES = False
 
 # Modify the number of Post per Index Page
 # Defaults to 10
